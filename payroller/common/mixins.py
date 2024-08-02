0d110c88c35b2e31f0507d08556f4e7e46e8a9a9
Phenyo Itumeleng <phenyoitumeleng@gmail.com>
2024-08-02 21:35:55 +0200

    added a base model mixin

---
+++ payroller/common/mixins.py
@@ -0,0 +1,14 @@
+from django import models
+
+from django.db import models
+from datetime import datetime
+
+
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
