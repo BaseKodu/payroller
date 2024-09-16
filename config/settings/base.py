7d7c6cc6a0ec0b83c56ab8fdf0a3e0e7c641565d
Phenyo Itumeleng <phenyoitumeleng@gmail.com>
2024-09-16 22:21:04 +0200

    Finished some configs

--- config/settings/base.py
+++ config/settings/base.py
@@ -92,6 +92,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "payroller.users",
+    "payroller.company",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
